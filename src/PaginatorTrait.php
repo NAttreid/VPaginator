ad39c46a492ce6c81d81de82b1a9e8e64f3fb079
attreid <attreid@gmail.com>
2016-09-06 10:30:54 +0200

    odsazeni

--- src/PaginatorTrait.php
+++ src/PaginatorTrait.php
@@ -2,44 +2,51 @@
 
 namespace NAttreid\VPaginator;
 
-trait PaginatorTrait {
+use Nette\Database\Table\Selection;
+use Nextras\Orm\Collection\ICollection;
 
-    /**
-     * Vytvori instanci paginatoru
-     * @param int $itemsPerPage
-     * @return VPaginator
-     */
-    public function createPaginator($itemsPerPage = 10) {
-        $control = new VPaginator();
-        $paginator = $control->getPaginator();
-        $paginator->itemsPerPage = $itemsPerPage;
-        return $control;
-    }
+trait PaginatorTrait
+{
 
-    /**
-     * Nastavi strankovani
-     * @param string $componentName
-     * @param $model
-     */
-    public function setPaginator($componentName, $model) {
-        $paginator = $this[$componentName]->getPaginator();
-        if ($model instanceof \Nette\Database\Table\Selection) {
-            $paginator->itemCount = $model->count();
-            $model->limit($paginator->itemsPerPage, $paginator->offset);
-        } elseif ($model instanceof \Nextras\Orm\Collection\ICollection) {
-            $paginator->itemCount = $model->countStored();
-            $model->limitBy($paginator->itemsPerPage, $paginator->offset);
-        }
-    }
+	/**
+	 * Vytvori instanci paginatoru
+	 * @param int $itemsPerPage
+	 * @return VPaginator
+	 */
+	public function createPaginator($itemsPerPage = 10)
+	{
+		$control = new VPaginator();
+		$paginator = $control->getPaginator();
+		$paginator->itemsPerPage = $itemsPerPage;
+		return $control;
+	}
 
-    /**
-     * Vrati aktualni stranku
-     * @param string $componentName
-     * @return int
-     */
-    public function getPage($componentName) {
-        $paginator = $this[$componentName]->getPaginator();
-        return $paginator->getPage();
-    }
+	/**
+	 * Nastavi strankovani
+	 * @param string $componentName
+	 * @param $model
+	 */
+	public function setPaginator($componentName, $model)
+	{
+		$paginator = $this[$componentName]->getPaginator();
+		if ($model instanceof Selection) {
+			$paginator->itemCount = $model->count();
+			$model->limit($paginator->itemsPerPage, $paginator->offset);
+		} elseif ($model instanceof ICollection) {
+			$paginator->itemCount = $model->countStored();
+			$model->limitBy($paginator->itemsPerPage, $paginator->offset);
+		}
+	}
+
+	/**
+	 * Vrati aktualni stranku
+	 * @param string $componentName
+	 * @return int
+	 */
+	public function getPage($componentName)
+	{
+		$paginator = $this[$componentName]->getPaginator();
+		return $paginator->getPage();
+	}
 
 }
--- src/VPaginator.php
+++ src/VPaginator.php
@@ -12,173 +12,183 @@ use Nette\Utils\Paginator;
  * @author Dusan Hudak
  * @author Attreid <attreid@gmail.com>
  */
-class VPaginator extends Control {
+class VPaginator extends Control
+{
 
-    /** @persistent */
-    public $page = 1;
+	/** @persistent */
+	public $page = 1;
 
-    /** @var array */
-    public $onShowPage;
+	/** @var array */
+	public $onShowPage;
 
-    /** @var int */
-    public $pageAround = 3;
+	/** @var int */
+	public $pageAround = 3;
 
-    /** @var int */
-    public $othersPage = 2;
+	/** @var int */
+	public $othersPage = 2;
 
-    /**
-     * Text na tlacitku predchozi
-     * @var string 
-     */
-    public $prev = '«';
+	/**
+	 * Text na tlacitku predchozi
+	 * @var string
+	 */
+	public $prev = '«';
 
-    /**
-     * Text na tlacitku dalsi
-     * @var string 
-     */
-    public $next = '»';
+	/**
+	 * Text na tlacitku dalsi
+	 * @var string
+	 */
+	public $next = '»';
 
-    /**
-     * Text na tlacitku ostatni
-     * @var string 
-     */
-    public $other = '...';
+	/**
+	 * Text na tlacitku ostatni
+	 * @var string
+	 */
+	public $other = '...';
 
-    /** @var string */
-    private $templateFile;
+	/** @var string */
+	private $templateFile;
 
-    /** @var Paginator */
-    private $paginator;
+	/** @var Paginator */
+	private $paginator;
 
-    /** @var bool */
-    private $isAjax = FALSE;
+	/** @var bool */
+	private $isAjax = FALSE;
 
-    /** @var bool */
-    private $noHistory = FALSE;
+	/** @var bool */
+	private $noHistory = FALSE;
 
-    public function __construct() {
-        parent::__construct();
+	public function __construct()
+	{
+		parent::__construct();
 
-        $reflection = $this->getReflection();
-        $dir = dirname($reflection->getFileName());
-        $this->templateFile = $dir . DIRECTORY_SEPARATOR . 'paginator.latte';
-    }
+		$reflection = $this->getReflection();
+		$dir = dirname($reflection->getFileName());
+		$this->templateFile = $dir . DIRECTORY_SEPARATOR . 'paginator.latte';
+	}
 
-    /**
-     * @param bool $value
-     * @return static 
-     */
-    public function setAjaxRequest($value = TRUE) {
-        $this->isAjax = $value;
-        return $this;
-    }
+	/**
+	 * @param bool $value
+	 * @return static
+	 */
+	public function setAjaxRequest($value = TRUE)
+	{
+		$this->isAjax = $value;
+		return $this;
+	}
 
-    /**
-     * @param bool $value
-     * @return static 
-     */
-    public function setNoAjaxHistory($value = TRUE) {
-        $this->noHistory = $value;
-        return $this;
-    }
+	/**
+	 * @param bool $value
+	 * @return static
+	 */
+	public function setNoAjaxHistory($value = TRUE)
+	{
+		$this->noHistory = $value;
+		return $this;
+	}
 
-    /**
-     * @return Paginator
-     */
-    public function getPaginator() {
-        if (!$this->paginator) {
-            $this->paginator = new Paginator;
-        }
-        return $this->paginator;
-    }
+	/**
+	 * @return Paginator
+	 */
+	public function getPaginator()
+	{
+		if (!$this->paginator) {
+			$this->paginator = new Paginator;
+		}
+		return $this->paginator;
+	}
 
-    /**
-     * @param int $page
-     */
-    public function handleShowPage($page) {
-        $this->onShowPage($this, $page);
-    }
+	/**
+	 * @param int $page
+	 */
+	public function handleShowPage($page)
+	{
+		$this->onShowPage($this, $page);
+	}
 
-    /**
-     * @return string
-     */
-    public function getTemplateFile() {
-        return $this->templateFile;
-    }
+	/**
+	 * @return string
+	 */
+	public function getTemplateFile()
+	{
+		return $this->templateFile;
+	}
 
-    /**
-     * @param string $file
-     * @return Vpaginator provides fluent interface
-     */
-    public function setTemplateFile($file) {
-        if ($file) {
-            $this->templateFile = $file;
-        }
-        return $this;
-    }
+	/**
+	 * @param string $file
+	 * @return Vpaginator provides fluent interface
+	 */
+	public function setTemplateFile($file)
+	{
+		if ($file) {
+			$this->templateFile = $file;
+		}
+		return $this;
+	}
 
-    /**
-     * Renders paginator.
-     * @return void
-     */
-    public function render() {
-        $paginator = $this->getPaginator();
-        $page = $paginator->page;
-        if ($paginator->pageCount < 2) {
-            $steps = [$page];
-            $viewed = FALSE;
-        } else {
-            $viewed = TRUE;
+	/**
+	 * Renders paginator.
+	 * @return void
+	 */
+	public function render()
+	{
+		$paginator = $this->getPaginator();
+		$page = $paginator->page;
+		if ($paginator->pageCount < 2) {
+			$steps = [$page];
+			$viewed = FALSE;
+		} else {
+			$viewed = TRUE;
 
-            $f = $first = $page - $this->pageAround;
-            $l = $last = $page + $this->pageAround;
-            if ($f < $paginator->firstPage) {
-                $last += ($paginator->firstPage - $f);
-            }
-            if ($l > $paginator->lastPage) {
-                $first -= ($l - $paginator->lastPage);
-            }
-            $arr = range(max($paginator->firstPage, $first), min($paginator->lastPage, $last));
+			$f = $first = $page - $this->pageAround;
+			$l = $last = $page + $this->pageAround;
+			if ($f < $paginator->firstPage) {
+				$last += ($paginator->firstPage - $f);
+			}
+			if ($l > $paginator->lastPage) {
+				$first -= ($l - $paginator->lastPage);
+			}
+			$arr = range(max($paginator->firstPage, $first), min($paginator->lastPage, $last));
 
-            if ($this->othersPage > 0) {
-                $count = $this->othersPage * 2;
-                $quotient = ($paginator->pageCount - 1) / $count;
-                for ($i = 0; $i <= $count; $i++) {
-                    $arr[] = round($quotient * $i) + $paginator->firstPage;
-                }
-                sort($arr);
-                $steps = array_values(array_unique($arr));
-            } else {
-                $steps = $arr;
-            }
-        }
+			if ($this->othersPage > 0) {
+				$count = $this->othersPage * 2;
+				$quotient = ($paginator->pageCount - 1) / $count;
+				for ($i = 0; $i <= $count; $i++) {
+					$arr[] = round($quotient * $i) + $paginator->firstPage;
+				}
+				sort($arr);
+				$steps = array_values(array_unique($arr));
+			} else {
+				$steps = $arr;
+			}
+		}
 
-        $this->template->viewed = $viewed;
-        $this->template->steps = $steps;
-        $this->template->paginator = $paginator;
-        $this->template->isAjax = $this->isAjax;
-        $this->template->noHistory = $this->noHistory;
-        $this->template->prev = $this->prev;
-        $this->template->next = $this->next;
-        $this->template->other = $this->other;
-        if (count($this->onShowPage) > 0) {
-            $this->template->handle = 'showPage!';
-        } else {
-            $this->template->handle = 'this';
-        }
+		$this->template->viewed = $viewed;
+		$this->template->steps = $steps;
+		$this->template->paginator = $paginator;
+		$this->template->isAjax = $this->isAjax;
+		$this->template->noHistory = $this->noHistory;
+		$this->template->prev = $this->prev;
+		$this->template->next = $this->next;
+		$this->template->other = $this->other;
+		if (count($this->onShowPage) > 0) {
+			$this->template->handle = 'showPage!';
+		} else {
+			$this->template->handle = 'this';
+		}
 
-        $this->template->setFile($this->getTemplateFile());
-        $this->template->render();
-    }
+		$this->template->setFile($this->getTemplateFile());
+		$this->template->render();
+	}
 
-    /**
-     * Loads state informations.
-     * @param  array
-     * @return void
-     */
-    public function loadState(array $params) {
-        parent::loadState($params);
-        $this->getPaginator()->page = $this->page;
-    }
+	/**
+	 * Loads state informations.
+	 * @param  array
+	 * @return void
+	 */
+	public function loadState(array $params)
+	{
+		parent::loadState($params);
+		$this->getPaginator()->page = $this->page;
+	}
 
 }
